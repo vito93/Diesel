ca69c0cd38a9cc80268e5f9b55d2f01fefba3580
Victor Klein <vikklein93@gmail.com>
2022-02-18 00:57:12 +0400

    Calendar table & seed script added.

---
+++ Diesel/Seed/Calendar.sql
@@ -0,0 +1,15 @@
+﻿USE Diesel
+GO
+
+DECLARE @startDate DATE = '20200101',
+		@endDate DATE = '20250101'
+
+DECLARE @i DATE = @startDate
+		
+WHILE @i < @endDate
+BEGIN
+	INSERT INTO Calendar(DateID)
+	SELECT cast(convert(NVARCHAR, @i, 112) as int)
+
+	SET @i = DATEADD(DAY, 1, @i)
+END
---
+++ Diesel/dbo/Tables/Calendar.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Calendar] (
+    [DateId]     INT NOT NULL,
+    [DateColumn] AS  (CONVERT([date],CONVERT([nvarchar],[DateId]),(112))),
+    CONSTRAINT [PK_Calendar] PRIMARY KEY CLUSTERED ([DateId] ASC)
+);
+
