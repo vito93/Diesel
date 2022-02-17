39c1d2396ad38e49f4878be044aaa8e1d011051c
Victor Klein <vikklein93@gmail.com>
2022-02-18 00:14:55 +0400

    Initial DB project commit; DieselJournal table added.

---
+++ Diesel/dbo/Tables/DieselJournal.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DieselJournal] (
+    [Id]                TINYINT       IDENTITY (1, 1) NOT NULL,
+    [diesel_machine_id] VARCHAR (50)  NULL,
+    [downtime_begin]    DATETIME2 (3) NULL,
+    [downtime_end]      DATETIME2 (3) NULL,
+    CONSTRAINT [PK_DieselJournal] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
